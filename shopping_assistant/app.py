141a7e8495a7d24cc3c329961f66f33204a3f02d
Tatianaob <ltatiobandore@gmail.com>
2025-02-11 16:02:42 -0800

    Updated file to test google search api

--- shopping_assistant/app.py
+++ shopping_assistant/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from serp_api import search_products
 
 app = Flask(__name__)
 
@@ -6,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/search", methods=["POST"])
+def search():
+    query = request.form["query"]
+    products = search_products(query)
+    return jsonify(products)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True) 
